8b5fae85dfa25d7c088dd9de4d115cc617b99238
IlijaP81 <niilp1999no@gmail.com>
2024-07-26 14:17:00 +0300

    Добавьте файлы проекта.

---
+++ JaggedArray/Program.cs
@@ -0,0 +1,16 @@
+﻿class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int[][] jaggedArray = [[1, 2], [1, 2, 3], [1, 2, 3, 4, 5]];
+
+        foreach (var i in jaggedArray)
+        {
+            foreach (var j in i)
+            {
+                Console.Write(j + " ");    
+
+            }
+        }
+    }
+}
